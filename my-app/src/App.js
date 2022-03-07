65181ef15a3ec3f687dde662af312eb7bd54b260
minkh0512 <minkh0512@naver.com>
2022-03-07 10:00:40 +0900

    짝맞추기게임

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,22 @@
-import { useState } from 'react';
-import './App.css';
-import CardGameTitle from './components/CardGameTitle'
-import CardGame from './components/CardGame';
-import card1 from './img/card1.png';
-import card2 from './img/card2.png';
-import card3 from './img/card3.png';
-import card4 from './img/card4.png';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './routes/Home';
+import CardMatchingGame from './routes/CardMatchingGame';
 
 const App = () =>{
-  const [start, setStart] = useState(false);
-  const [reStart, setReStart] = useState(false);
-
-  function startClickHandler(boolean){
-    setStart(boolean);
-  }
-  
-  return (
-    <div className="app-card-matching">
-      <CardGameTitle start={start} startClickHandler={startClickHandler} />
-      {
-        start 
-        ? 
-        <CardGame />
-        :
-        <>
-          <img src={card1} alt="" className="bg_ready_img bg_ready_img1" />
-          <img src={card2} alt="" className="bg_ready_img bg_ready_img2" />
-          <img src={card3} alt="" className="bg_ready_img bg_ready_img3" />
-          <img src={card4} alt="" className="bg_ready_img bg_ready_img4" />
-        </>
-      }
-      
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/CardMatchingGame">
+        <CardMatchingGame />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 }
 
 export default App;
