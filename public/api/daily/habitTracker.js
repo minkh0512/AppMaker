459c5afeb939466b5552785768bec2ca86e66d60
minkh0512 <minkh0512@naver.com>
2022-03-11 18:26:32 +0900

    habitTracker

---
+++ public/api/daily/habitTracker.js
@@ -0,0 +1,115 @@
+
+const habitTrackerApi = [
+  {
+    title: '코딩하기',
+    days: [
+      {
+        day: 1,
+        isComplete: false
+      },
+      {
+        day: 2,
+        isComplete: false
+      },
+      {
+        day: 3,
+        isComplete: false
+      },
+      {
+        day: 4,
+        isComplete: false
+      },
+      {
+        day: 5,
+        isComplete: false
+      },
+      {
+        day: 6,
+        isComplete: false
+      },
+      {
+        day: 7,
+        isComplete: false
+      },
+      {
+        day: 8,
+        isComplete: false
+      },
+      {
+        day: 9,
+        isComplete: false
+      },
+      {
+        day: 10,
+        isComplete: false
+      },
+      {
+        day: 11,
+        isComplete: false
+      },
+      {
+        day: 12,
+        isComplete: false
+      },
+      {
+        day: 13,
+        isComplete: false
+      }
+    ]
+  },
+  {
+    title: '놀기',
+    days: [
+      {
+        day: 1,
+        isComplete: false
+      },
+      {
+        day: 2,
+        isComplete: false
+      },
+      {
+        day: 3,
+        isComplete: false
+      },
+      {
+        day: 4,
+        isComplete: false
+      },
+      {
+        day: 5,
+        isComplete: false
+      },
+      {
+        day: 6,
+        isComplete: false
+      },
+      {
+        day: 7,
+        isComplete: false
+      },
+      {
+        day: 8,
+        isComplete: false
+      },
+      {
+        day: 9,
+        isComplete: false
+      },
+      {
+        day: 10,
+        isComplete: false
+      },
+      {
+        day: 11,
+        isComplete: false
+      },
+      {
+        day: 12,
+        isComplete: false
+      }
+    ]
+  }
+];
+
+export default habitTrackerApi
